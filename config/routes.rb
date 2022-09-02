b3e48f471d31224616777498af31f1f114081f98
peppino708 <708.baseball.ster@gmail.com>
2022-09-02 14:23:40 +0900

    add link and create destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
-  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
+  resources :articles
 end
